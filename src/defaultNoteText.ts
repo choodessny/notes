7609598158110cd3f8fa56701a59507ecde2e793
choodessny <advekud@gmail.com>
2023-03-06 00:16:21 +0400

    update default note text

--- src/defaultNoteText.ts
+++ src/defaultNoteText.ts
@@ -2,5 +2,7 @@ export const DEFAULT_NOTE_TEXT = `# Тестовое задание
 
 ## Кудрявцева Анастасия
 
+![GitHub top language](https://img.shields.io/github/languages/top/choodessny/notes) ![GitHub package.json dependency version (prod)](https://img.shields.io/github/package-json/dependency-version/choodessny/notes/react) ![GitHub package.json dependency version (prod)](https://img.shields.io/github/package-json/dependency-version/choodessny/notes/@mui/material)
+
 - [ссылка на репозиторий](https://github.com/choodessny/notes)
 - [ссылка на резюме](https://hh.ru/resume/0eb39753ff0bb513360039ed1f546572397653)`;
